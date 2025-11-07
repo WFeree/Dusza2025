32551f5806663f799eabf343f8daa43937059554
Zoli0217 <zoltanbodi61@gmail.com>
2025-11-07 15:21:52 +0100

    login design

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -14,7 +14,7 @@ import { Label } from "@/components/ui/label"
 export function Login() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-background">
-        <Card className="w-full max-w-sm ">
+        <Card className="w-full max-w-sm">
             <CardHeader>
                 <CardTitle>Jelentkezz be a fiókodba</CardTitle>
                 <CardDescription>
