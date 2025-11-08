70b7ce6acc4236c1cc8da0ec60ec5ecd52bced58
Szentjobi Gergo <gszentjobi@gmail.com>
2025-11-08 17:56:53 +0100

    fight

--- backend/damareen/urls.py
+++ backend/damareen/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views.views import CardListCreate, GameListCreate, DungeonListCreate, PlayerCardListCreate
+from .views.views import CardListCreate, GameListCreate, DungeonListCreate, PlayerCardListCreate, ChallangeDungeon
 
 urlpatterns = [
     path('cards/', CardListCreate.as_view(), name='cards'),
     path('games/', GameListCreate.as_view(), name='games'),
     path('dungeons/', DungeonListCreate.as_view(), name='dungeons'),
+    path('dungeons/challange/', ChallangeDungeon, name='dungeons'),
     path('playerCards/', PlayerCardListCreate.as_view(), name='playerCards'),
 ]
